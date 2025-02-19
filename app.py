a63cbf31a519a6318c7decbc4abf7f9ee1cca9ee
rishav <rishav78397@gmail.com>
2025-02-19 10:53:14 +0530

    First

--- app.py
+++ app.py
@@ -5,25 +5,23 @@ from PIL import Image
 import numpy as np
 
 # Path to your Keras model
-model_path = 'model.keras'
+model_path = 'model 2.keras'
 
 # Load the Keras model
 model = load_model(model_path)
 
+# Function to preprocess the uploaded image
 def preprocess_image(image, target_size):
     # Convert the image to grayscale (if your model expects grayscale images)
-    image = image.convert('L')  # 'L' mode converts to grayscale (1 channel)
+    image = image.convert('L')
     # Resize the image to the target size
     image = image.resize(target_size)
     # Convert image to numpy array and normalize
-    image_array = np.array(image) / 255.0  # Normalize pixel values
+    image_array = np.array(image) / 255.0
     # Reshape the image to (1, target_size[0], target_size[1], 1) for grayscale
-    image_array = np.expand_dims(image_array, axis=-1)  # Add channel dimension for grayscale
-    image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
+    image_array = np.expand_dims(image_array, axis=-1)
+    image_array = np.expand_dims(image_array, axis=0)
     return image_array
-
-
-
 
 # Streamlit app UI
 st.title("Image Classification App")
@@ -38,7 +36,7 @@ if uploaded_file is not None:
 
     # Preprocess the uploaded image
     image = Image.open(uploaded_file)
-    target_size = (128, 128)  # Update this based on your model's input size
+    target_size = (150, 150)  # Update this based on your model's input size
     image_array = preprocess_image(image, target_size)
 
     # Make prediction
